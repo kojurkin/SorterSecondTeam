4b0d4837faecbae3bf2f1172d542d88445fc72ec
ilyakayukin <ilyakay30@gmail.com>
2025-12-13 17:15:49 +0600

    Обновление ветки, меню больше не статик, поправил тесты немного, теперь используется кастомная коллекция

--- src/main/java/org/example/menu/Menu.java
+++ src/main/java/org/example/menu/Menu.java
@@ -18,7 +18,6 @@ import org.example.utilites.Exporter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
