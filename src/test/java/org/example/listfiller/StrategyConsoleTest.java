6f5614f29a081856abdacfe96c98f8d617d746e9
ormocrion <ormocrion@gmail.com>
2025-12-13 23:32:59 +0300

    feat: Тестирует консольный заполнитель листа
    Signed-off-by: ormocrion <ormocrion@gmail.com>

--- src/test/java/org/example/listfiller/StrategyConsoleTest.java
+++ src/test/java/org/example/listfiller/StrategyConsoleTest.java
@@ -1,9 +1,8 @@
 package org.example.listfiller;
 
-import org.example.student.Student;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
+import java.io.StringReader;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,9 +11,80 @@ public class StrategyConsoleTest {
     StrategyConsole strategyConsole = new StrategyConsole();
 
     @Test
-    public void unexpectedParameter() throws Exception {
+    public void unexpectedParameter() {
         assertThrows(Exception.class, () -> {
             strategyConsole.fill(-1);
         });
+    }
+
+    @Test
+    public void tryParseStringToInt() {
+        String input = "abc";
+        Scanner scanner = new Scanner(new StringReader(input));
+        assertEquals(0, strategyConsole.parseToInt(scanner));
+    }
+
+    @Test
+    public void tryParseEmptyValueToInt() {
+        String input = "";
+        Scanner scanner = new Scanner(new StringReader(input));
+        assertEquals(0, strategyConsole.parseToInt(scanner));
+    }
+
+    @Test
+    public void tryParseIntToInt() {
+        String input = "1";
+        Scanner scanner = new Scanner(new StringReader(input));
+        assertEquals(1, strategyConsole.parseToInt(scanner));
+    }
+
+    @Test
+    public void tryParseDoubleToInt() {
+        String input = "2.2";
+        Scanner scanner = new Scanner(new StringReader(input));
+        assertEquals(0, strategyConsole.parseToInt(scanner));
+    }
+
+    @Test
+    public void tryParseNegativeValue() {
+        String input = "-1";
+        Scanner scanner = new Scanner(new StringReader(input));
+        assertEquals(-1, strategyConsole.parseToInt(scanner));
+    }
+
+    @Test
+    public void tryParseStringToDouble() {
+        String input = "abc";
+        assertEquals(0.0,strategyConsole.parseToDouble(input));
+    }
+
+    @Test
+    public void tryParseEmptyValueToDouble() {
+        String input = "";
+        assertEquals(0.0, strategyConsole.parseToDouble(input));
+    }
+
+    @Test
+    public void tryParseIntToDouble() {
+        String input = "1";
+        assertEquals(1.0, strategyConsole.parseToDouble(input));
+    }
+
+    @Test
+    public void tryParseDoubleToDouble() {
+        String input = "3,525216515661";
+        assertEquals(3.53, strategyConsole.parseToDouble(input));
+    }
+
+    @Test
+    public void tryParseDoubleWithCommaSeparator() {
+        String input = "4,324";
+        assertEquals(4.33, strategyConsole.parseToDouble(input));
+    }
+
+    @Test
+    public void tryParseNegativeValueToDouble() {
+        String input = "-2,23";
+        assertEquals(-2.23, strategyConsole.parseToDouble(input));
     }
 }
